decd660e8309d426d8b4425520c75b0a21c54489
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-08 14:46:46 -0800

    contact entity

---
+++ src/main/java/net/javamachine/contactApp/contact/Contact.java
@@ -0,0 +1,47 @@
+package net.javamachine.contactApp.contact;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Contact {
+
+    @Id
+    String contactId;
+    String name;
+    String mobileNumber;
+
+    public Contact() {
+    }
+
+    public Contact(String contactId, String name, String mobileNumber) {
+        this.contactId = contactId;
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+}
